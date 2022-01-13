c1280f0f1bc0871c09793631aec9f72e00196ec5
vlaskinmac <kangelina86@mail.ru>
2022-01-13 18:26:20 +0300

    Added readme

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,52 @@
+import datetime
+
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
-def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+def get_duration(visit):
+    duration_secs = visit.leaved_at - visit.entered_at
+    return duration_secs.seconds
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+
+def format_duration(duration):
+    hours, remainder_secs = divmod(duration, 3600)
+    minutes, seconds = divmod(remainder_secs, 60)
+    duration_min = duration // 60
+    duration_formated = '{:02} часов {:02} минут {:02} секунд'.format(
+        int(hours),
+        int(minutes),
+        int(seconds),
+    )
+    return duration_formated, duration_min
+
+
+def is_visit_long(visit, minutes=60):
+    if visit.leaved_at:
+        duration_secs = visit.leaved_at - visit.entered_at
+        _, duration = format_duration(duration_secs.seconds)
+        if duration < minutes:
+            return False
+
+
+def passcard_info_view(request, passcode):
+    passcard = Passcard.objects.filter(visit__passcard__passcode=passcode)
+    visits = Visit.objects.filter(passcard__passcode=passcode)
+    this_passcard_visits = []
+    for visit in visits:
+        if visit.leaved_at:
+            is_strange = is_visit_long(visit, minutes=60)
+            if type(visit.entered_at) == datetime.datetime:
+                duration_secs = get_duration(visit)
+                duration_formated, _ = format_duration(duration_secs)
+                this_passcard_visits.append(
+                    {
+                        'entered_at': visit.entered_at,
+                        'duration': duration_formated,
+                        'is_strange': is_strange
+                    },
+                )
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -10,35 +10,45 @@ def get_duration(visit):
         duration = localtime() - visit
         return duration.seconds
     else:
-        return None
+        return
 
 
 def format_duration(duration):
     hours, remainder_secs = divmod(duration, 3600)
     minutes, seconds = divmod(remainder_secs, 60)
-    return '{:02} часов {:02} минут {:02} секунд'.format(
+    duration_min = duration // 60
+    duration_formated = '{:02} часов {:02} минут {:02} секунд'.format(
         int(hours),
         int(minutes),
         int(seconds),
     )
+    return duration_formated, duration_min
+
+
+def is_visit_long(visit, minutes=60):
+    if visit.leaved_at:
+        duration_secs = visit.leaved_at - visit.entered_at
+        _, duration = format_duration(duration_secs.seconds)
+        if duration < minutes:
+            return False
 
 
 def storage_information_view(request):
-
-    visits = Visit.objects.all()
+    visits = Visit.objects.order_by("entered_at")
     non_closed_visits = []
     for visit in visits:
+        is_visit_long(visit, minutes=60)
         if not visit.leaved_at:
-            if type(visit.entered_at) == datetime.datetime:
-                duration_secs = get_duration(visit.entered_at)
-                if duration_secs:
-                    duration = format_duration(duration_secs)
-                    non_closed_visits.append({
-                        'who_entered': visit.passcard.owner_name,
-                        'entered_at': visit.entered_at,
-                        'duration': duration,
-                    }
-                    )
+            duration_secs = get_duration(visit.entered_at)
+            if duration_secs:
+                duration_formated, _ = format_duration(duration_secs)
+                non_closed_visits.append({
+                    'who_entered': visit.passcard.owner_name,
+                    'entered_at': visit.entered_at,
+                    'duration': duration_formated,
+
+                }
+                )
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
