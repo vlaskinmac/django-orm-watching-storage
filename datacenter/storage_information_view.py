efb868c4dfe172fda0737f782b569e6443099cef
vlaskinmac <kangelina86@mail.ru>
2022-01-15 09:41:24 +0300

    Fixed  filter

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -34,21 +34,20 @@ def is_visit_long(visit, minutes=60):
 
 
 def storage_information_view(request):
-    visits = Visit.objects.order_by("entered_at")
+    visits = Visit.objects.filter(leaved_at=None)
     non_closed_visits = []
     for visit in visits:
         is_visit_long(visit, minutes=60)
-        if not visit.leaved_at:
-            duration_secs = get_duration(visit.entered_at)
-            if duration_secs:
-                duration_formated, _ = format_duration(duration_secs)
-                non_closed_visits.append({
-                    'who_entered': visit.passcard.owner_name,
-                    'entered_at': visit.entered_at,
-                    'duration': duration_formated,
+        duration_secs = get_duration(visit.entered_at)
+        if duration_secs:
+            duration_formated, _ = format_duration(duration_secs)
+            non_closed_visits.append({
+                'who_entered': visit.passcard.owner_name,
+                'entered_at': visit.entered_at,
+                'duration': duration_formated,
 
-                }
-                )
+            }
+            )
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
