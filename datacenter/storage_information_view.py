c798ebf942becfaef023bd3f9a4f83147e9138bb
vlaskinmac <kangelina86@mail.ru>
2022-01-19 10:32:19 +0300

    Removed get_duration_who_in_storage_now()

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,23 +1,14 @@
-from datacenter.duration_calculate import is_visit_long, format_duration
+from datacenter.duration_calculate import get_duration, format_duration
 
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.utils.timezone import localtime
-
-
-def get_duration_who_in_storage_now(visit):
-    if visit:
-        duration = localtime() - visit
-        return duration.total_seconds()
-    else:
-        return
 
 
 def storage_information_view(request):
     visits = Visit.objects.order_by('entered_at').filter(leaved_at=None)
     non_closed_visits = []
     for visit in visits:
-        duration_secs = get_duration_who_in_storage_now(visit.entered_at)
+        duration_secs = get_duration(visit)
         if duration_secs:
             duration_formated, _ = format_duration(duration_secs)
             non_closed_visits.append({
